72859d1ce29ec9f4bf359ce5a39098d33b4981e8
kapilsaini773402 <kapilsaini773402@gmail.com>
2025-03-08 15:25:07 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,7 +1,7 @@
 from os import getenv
 
-api_id    = int(getenv("api_id", ""))
-api_hash  = getenv("api_hash", "")
+api_id    = int(getenv("api_id", "21761132"))
+api_hash  = getenv("api_hash", "595b707f4683b3e7034a6a6fc3fe5eb2")
 bot_token = getenv("bot_token", "")
 pwtoken = getenv("pwtoken", "")
 CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
